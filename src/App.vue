4048d62fdd489a839a921b6459891e5ca2960af9
levanakhalaia <131708318+levanakhalaia@users.noreply.github.com>
2023-04-24 23:12:45 +0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Register />
+</template>
+
+<script>
+import CardGame from './components/cardgame.vue'
+import Register from './components/register.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CardGame,
+    Register
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
